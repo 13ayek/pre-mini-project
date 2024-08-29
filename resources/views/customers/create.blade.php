0457e13f11dc9c22011857fe51ba5413af4ec62f
ardiermdhn <ardiermdhn0510@gmail.com>
2024-08-29 14:55:28 +0700

    customer index

---
+++ resources/views/customers/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+@section('content')
+    <main>
+
+        <div class="container mt-5">
+            <div class="container">
+                <a href="{{ route('customers.create') }}" type="button" class="btn btn-outline-info mb-3">Tambah</a>
+            </div>
+            <table class="table table-bordered border-primary">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($customers as $customer)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $customer->name }}</td>
+                            <td>{{ $customer->email }}</td>
+                            <td>{{ $customer->phone }}</td>
+                            <td>{{ $customer->address }}</td>
+                            <td>
+                                <a href="{{ route('customers.edit', $customer->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('customers.destroy', $customer->id) }}" method="POST"
+                                    class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Apakah Anda yakin ingin menghapus penumpang ini?');">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/customers/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('content')
+    <main>
+        <div class="card">
+            <div class="card-body">
+                <h1>CleanDream</h1>
+                <p>make your clothes clean like a dream</p>
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <div class="container">
+                <a href="{{ route('customers.create') }}" type="button" class="btn btn-outline-info mb-3">Tambah</a>
+            </div>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($customers as $customer)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $customer->name }}</td>
+                            <td>{{ $customer->email }}</td>
+                            <td>{{ $customer->phone }}</td>
+                            <td>{{ $customer->address }}</td>
+                            <td>
+                                <a href="{{ route('customers.edit', $customer->id) }}"
+                                    class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('customers.destroy', $customer->id) }}" method="POST"
+                                    class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Apakah Anda yakin ingin menghapus penumpang ini?');">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </main>
+@endsection
