48166e7a2d73d48c141e02e60f6f43055b6b713b
13ayek <ryutaiyang@gmail.com>
2024-08-29 09:41:43 +0700

    membuat migration model controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::resource([
+    'customers' => CustomerController::class,
+]);
